f3de9677ad7bf16c62200deb596116c0a4d517ba
boldtrn <boldtrn@gmail.com>
2016-09-18 18:21:19 +0200

    Render more intense Hillshading

--- Hillup/__init__.py
+++ Hillup/__init__.py
@@ -88,7 +88,7 @@ def arr2img(ar):
     arA = ar
 
     for index,value in numpy.ndenumerate( arA ):
-        value = .3 * fabs(128 - value)
+        value = 1.9 * fabs(128 - value)
 
         arA[index] = value
 
