74666f0f65f5c62d7ae2bddbac4da1108883e71d
boldtrn <boldtrn@gmail.com>
2016-09-18 13:55:05 +0200

    Add Alpha Channel

--- Hillup/__init__.py
+++ Hillup/__init__.py
@@ -86,7 +86,7 @@ def arr2img(ar):
     """
     g = Image.fromstring('L', (ar.shape[1], ar.shape[0]), ar.astype('b').tostring())
 
-    return Image.fromstring('RGBA', (ar.shape[1], ar.shape[0]), (g,g,g,g))
+    return Image.frombuffer('RGBA', (ar.shape[1], ar.shape[0]), (g,g,g,g))
 
 def slope2bytes(slope):
     """ Convert slope from floating point to 8-bit.
