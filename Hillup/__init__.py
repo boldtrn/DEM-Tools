b11ee0aa21a044b26ef255a697456d78df3ecc0f
boldtrn <boldtrn@gmail.com>
2016-09-18 19:12:41 +0200

    Render more intense Hillshading

--- Hillup/__init__.py
+++ Hillup/__init__.py
@@ -97,7 +97,8 @@ def arr2img(ar):
     for y in xrange(img.size[1]):
         for x in xrange(img.size[0]):
                 val = pixdata[x, y][0]
-                pixdata[x, y] = (val, 255)
+                op = 2*(fabs(128-val))
+                pixdata[x, y] = (val, op)
 
     return img
 
